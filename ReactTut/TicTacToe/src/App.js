82ff69d60587d5cf331e751770da2fdea39c1a25
Kacy R Gilbert <100252391+Remontz@users.noreply.github.com>
2023-02-01 03:00:40 +0000

    made changes...deleted changes..

--- ReactTut/TicTacToe/src/App.js
+++ ReactTut/TicTacToe/src/App.js
@@ -1,27 +1,3 @@
-import React from "react"
-import parse from 'html-react-parser'
-
-
-export default function Board() {
-  const parse = require('html-react-parser')
-  const buildSquares = (rowMultiplier) => {
-    for (let t = 1; t <= 3; t++) {
-      parse("<button className='square'>{t*rowMultiplier}</button>")
-      // <button className="square">{t * rowMultiplier}</button>
-    }
-  }
-  const makeBoard = () => {
-    for (let i = 0; i < 3; i++) {
-      parse("<div className='board-row'>")
-      {
-        buildSquares(i + 1)
-      }
-      parse("</div>")
-    }
-  }
-  return (
-    <main>
-      {makeBoard()}
-    </main>
-  )
+export default function Square() {
+  return <button className="square">X</button>;
 }
