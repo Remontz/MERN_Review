969ec3f04ae18bc554ec74883d054867c64832de
Kacy R Gilbert <100252391+Remontz@users.noreply.github.com>
2023-02-03 04:45:32 +0000

    Started React Blocks Project

---
+++ FunctionalComponents/react-blocks/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Header from './components/Header'
+import Navigation from './components/Navigation'
+import Main from './components/Main'
+import SubContents from './components/SubContents'
+import Advertisement from './components/Advertisement'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
