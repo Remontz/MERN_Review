8e5b15170753ff065566968d9395ed3a49be0c2a
Kacy R Gilbert <100252391+Remontz@users.noreply.github.com>
2023-02-07 04:13:14 +0000

    Tabtastic like a boss!!

--- FunctionalComponents/tabs/src/Components/Tabs.js
+++ FunctionalComponents/tabs/src/Components/Tabs.js
@@ -4,7 +4,7 @@ const Tabs = (props) => {
     const { arrayLength } = props
     const blankArray = new Array(arrayLength).fill(null)
     for (let i = 0; i < arrayLength; i++) {
-        blankArray[i] = { label: ('Tab ' + (i + 1)), content: ('Tab ' + (i + 1) + ' content is showing here.') }
+        blankArray[i] = { label: ('Tab ' + (i + 1)), content: ('Tab ' + (i + 1) + ' content is showing here.'), btnId: '' }
     }
 
     const [tabsArray, setTabsArray] = useState(blankArray)
@@ -20,12 +20,22 @@ const Tabs = (props) => {
     }
 
     const [mousedOver, setMousedOver] = useState(false)
-    const handleMouseOver = (e) => {
+    const handleMouseOver = (e, index) => {
         setMousedOver(true)
+        const newArray = tabsArray.filter((tab, i) => (
+            (i === index ? tab.btnId = 'dark-btn' : tab.btnId = 'btn')
+        ))
+        setTabsArray(newArray)
     }
-    const handleMouseLeave = (e) => {
+    const handleMouseLeave = (e, index) => {
         setMousedOver(false)
+        const newArray = tabsArray.filter((tab, i) => (
+            (i === index ? tab.btnId = 'btn' : tab.btnId = 'btn')
+        ))
+        setTabsArray(newArray)
     }
+
+
 
 
     return (
@@ -33,11 +43,11 @@ const Tabs = (props) => {
             {<div className='tabs-btns'>
                 {tabsArray.map((tab, index) => (
                     <button
-                        id={mousedOver ? 'dark-btn' : 'btn'}
+                        id={tab.btnId}
                         //To fix maybe add button-id as an attribute to the object.
                         onClick={() => tabClicked(index)}
-                        onMouseOver={(e) => handleMouseOver(e)}
-                        onMouseLeave={(e) => handleMouseLeave(e)}
+                        onMouseOver={(e) => handleMouseOver(e, index)}
+                        onMouseLeave={(e) => handleMouseLeave(e, index)}
                     >
                         {tab.label}
                     </button>
