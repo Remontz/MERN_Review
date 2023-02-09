2578f952c0a218e5e16063f8e4a6501199bd4181
Kacy R Gilbert <100252391+Remontz@users.noreply.github.com>
2023-02-09 03:01:12 +0000

    demo

---
+++ Example/how-to-view-pdf/src/PDFViewer.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+
+
+function PDFViewer() {
+
+    return (
+        <div>
+
+        </div>
+        //The API version "3.3.122" does not match the Worker version "2.15.349".
+    )
+}
+
+export default PDFViewer
