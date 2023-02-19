c25e4d3ce310ac4f442550df46650abb4a527341
Kacy R Gilbert <100252391+Remontz@users.noreply.github.com>
2023-02-19 03:33:43 +0000

    more routing

--- ReactRouting/routing-practice/src/App.js
+++ ReactRouting/routing-practice/src/App.js
@@ -1,18 +1,37 @@
-import './App.css';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
-import About from './components/About';
-import Home from './components/Home';
-
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+//Remember, we can write these functional components in their own files 
+    //to be exported and imported to the App component.
+    //We've combined them here to simplify our example.
+const Home = (props) => { 
+  return (
+    <h1>Home Component</h1>
+  );
+}
+    
+const About = (props) => {
+  return (
+    <h1>About Component</h1>
+  );
+}
+    
 function App() {
   return (
     <BrowserRouter>
+    <div className="App">
       <h1>Routing Example</h1>
       <Routes>
-        <Route path='/about' element={<About />} />
-        <Route exact path='/' element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route exact path="/" element={<Home />} />
       </Routes>
+    </div>
     </BrowserRouter>
   );
 }
-
-export default App;
+    
+export default App
